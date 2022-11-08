64e1bddd4701640e9ceb5f65166821583192f81a
ale vel <ale.vrs@outlook.com>
2022-11-07 21:39:48 -0500

    Enviar parametros a otro archivo

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,9 @@
     <!-- BOOTSTRAP 4 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
