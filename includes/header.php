f3b227ba1456310c0e9efe29e1a237e87e0c9513
ale vel <ale.vrs@outlook.com>
2022-11-07 22:37:44 -0500

    Agregamos iconos con Fontawesome

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,9 @@
 
     <!-- BOOTSTRAP 4 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- FONT AWESOME 5 -->
+    <script src="https://kit.fontawesome.com/eec9feb0a4.js" crossorigin="anonymous"></script>
 </head>
 
 <nav class="navbar navbar-dark bg-dark">
